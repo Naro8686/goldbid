881a7a37be0bc951ad573a1f080052fe1cb7bc6e
naro8686 <narek199121@gmail.com>
2020-07-31 13:58:13 +0400

    add bonus modal view

---
+++ resources/views/site/include/bonus_modal.blade.php
@@ -0,0 +1,13 @@
+<div class="notify__modal">
+    <div class="notify notify__item">
+        <div class="bonus__30"></div>
+        <button class="notify__modal__btn__close btn__close">X</button>
+        <div class="notify__modal__body">
+            <h3>Заполни персональные данные и получи на счет</h3>
+            <h2><span>{{$bonus}} </span>Бонусов</h2>
+        </div>
+        <div class="notify__modal__footer">
+            <a href="{{route('profile.personal')}}" class="button__app">Получить</a>
+        </div>
+    </div>
+</div>
